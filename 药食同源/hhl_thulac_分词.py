b70eaf31c3aadb9396c167add1b8c76d038953a7
hhl <honghailhhl@163.com>
2021-02-08 17:50:19 +0800

    计算时间的分词

--- 药食同源/hhl_thulac_分词.py
+++ 药食同源/hhl_thulac_分词.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python 
-#coding:utf-8 
-import sys  
-import pandas as pd 
-import thulac   
-import codecs
-reload(sys)  
-sys.setdefaultencoding('utf8')
-df = pd.read_csv('分词以后的药食同源.csv',encoding = 'utf-8')
+import pandas as pd   
+import thulac
+import sys
+import time
+df = pd.read_csv('人工分词的药食同源.csv',encoding = 'utf-8')
 def vob ():
     """
     Python 消除换行符一定.replace('\n','').replace('\r','') 
@@ -16,16 +12,21 @@ def vob ():
         vocabL = f.readlines() #读取的结果是list 
         vocabSet = []
         for i in vocabL:#删除换行符
-            i = i.replace('\n','').replace('\r','').replace('t','')
-            vocabSet.append(i)
+            try:
+                i = i.replace('\n','').replace('\r','')
+                vocabSet.append(i)
+            except:
+                continue
         f.close()
         return  vocabSet 
 
 vocabSet = vob()
-abstract = df.iloc[:,-3]
+abstract = df['摘要']
 # print(abstract) #打印正确，没啥问题
 def cut (abstract,vocabSet):
-    thul = thulac.thulac(user_dict ='中药大词典合集.txt',  seg_only=True)  #只进行分词，不进行词性标注
+    time_start = time.time()
+    # thul = thulac.thulac(seg_only=True)
+    thul = thulac.thulac(user_dict ='中药大词典合集.txt', seg_only=True)  #只进行分词，不进行词性标注
     c = []
     for ab in abstract:#把每个文章的摘要单独提取出来
         strs = [] #把每个文章的用到的药材提取出来放在列表中
@@ -37,25 +38,23 @@ def cut (abstract,vocabSet):
         for content in contents.split():
             # print(content)
         # exit()
-            if content  in vocabSet and content != '药食同源' and content.encode('utf-8') not in strs:  #判断是否在词表里面，提取中药材的关键词
+            if content  in vocabSet and content != '药食同源' and content not in strs:  #判断是否在词表里面，提取中药材的关键词
                 # print(content)
                 strs.append(content)
                 # print(strs)
-        
-                
-            # for i in strs:
-            #     print(i.decode("string_escape"))
-            #     exit()
-        strs = str(strs).replace("'",'').replace('[','').replace(']','').decode("string_escape")#将其转化为一个一维的列表目的就是为转化为DataFrame类型
-        #这里.decode("string_escape")为了将list中的Unicode编码转化为中文
+                # exit()
+        strs = str(strs).replace("'",'').replace('[','').replace(']','')#将其转化为一个一维的列表目的就是为转化为DataFrame类型
         c.append(strs)#将每一行分词结果添加进list
         strs = []
+    time_end = time.time()
+    time_sum = time_end-time_start
+    print(time_sum)
+    # 没引进词表的分词时间 18.486303329467773
+    # 引进词表的分词时间  30.582738161087036
     return  c
 text = cut(abstract,vocabSet)
+# print(text)
 thulacResult = pd.DataFrame(text)
 df['thulac引进词表的分词'] = thulacResult
-df.to_csv('分词以后的药食同源.csv',encoding = 'utf-8')
+df.to_csv('人工分词的药食同源.csv',encoding = 'utf-8')
 print('写入成功')
-# for j in text:
-#         print(j)
-#         # exit()
--- 药食同源/ltp分词.py
+++ 药食同源/ltp分词.py
@@ -7,5 +7,5 @@ ltp = LTP()
 
 # user_dict.txt 是词典文件， max_window是最大前向分词窗口
 ltp.init_dict(path="中药大词典合集.txt", max_window=4)
-segment, _ = ltp.seg(["他叫汤姆去拿外衣。"])
+# segment, _ = ltp.seg(["他叫汤姆去拿外衣。"])
 print(segment)
